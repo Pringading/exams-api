647c721de5b3995d6c5b023e95c00e3d1acd4a41
Pringading <jess.marcell@gmail.com>
2025-01-22 14:58:01 +0000

    add test to check dataframe has the expected columns

--- db/utils/edexcel_data.py
+++ db/utils/edexcel_data.py
@@ -6,5 +6,17 @@ EXCEL_GCSE_DATA = "db/data/Edexcel_GCSE.xlsx"
 
 
 def excel_to_df(filepath: str, sheet:int=2) -> pd.DataFrame:
+    """Takes excel sheet and converts to a dataframe.
+    
+    Args:
+        filepath (str): path to the excel file.
+        sheet (int): within the excel workbook, the number of the sheet to 
+            obtain the data from. Sheets are 0-indexed & the same order as the 
+            tabs along the bottom.
+
+    Returns:
+        Data from the sheet as a pandas dataframe.
+    """
+    
     df = pd.read_excel(filepath, sheet_name=sheet)
     return df
--- test/test_edexcel_data.py
+++ test/test_edexcel_data.py
@@ -2,10 +2,28 @@ from db.utils.edexcel_data import excel_to_df
 from pandas import DataFrame
 import pytest
 
-@pytest.mark.it('Test excel to df function returns a dataframe')
-def test_excel_to_df_returns_df():
-    result = excel_to_df("db/data/Edexcel_GCE.xlsx")
-    assert isinstance(result, DataFrame)
+
+class TestExcelToDF:
+    """Testing excel to df function from the db/utils/edexcel_data.py workbook"""
+
+    @pytest.mark.it('Test excel to df function returns a dataframe')
+    def test_excel_to_df_returns_df(self):
+        """Testing that the return value is a pandas dataframe"""
+
+        result = excel_to_df("db/data/Edexcel_GCE.xlsx")
+        assert isinstance(result, DataFrame)
 
 
+    @pytest.mark.it('Test excel to df returns dataframe with the expected columns')
+    def test_excel_to_df_columns(self):
+        """Testing that the returned dataframe has the expected columns"""
 
+        expected_columns = [
+            'Date', 'Exam series', 'Board', 'Examination code',
+            'Subject', 'Title', 'Time', 'Duration'
+        ]
+        result = excel_to_df("db/data/Edexcel_GCE.xlsx")
+        for column in expected_columns:
+            assert column in result.columns
+        
+        
