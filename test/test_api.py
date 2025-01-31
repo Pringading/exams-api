ac1c8c360515e608a185639b61f5901ad00af3b9
Pringading <jess.marcell@gmail.com>
2025-01-31 14:27:40 +0000

    create api healthcheck function

---
+++ test/test_api.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+import pytest
+from main import app
+from db.seed import seed_db
+
+@pytest.fixture
+def seed_database(autouse=True):
+    seed_db()
+
+@pytest.fixture
+def test_client():
+    return TestClient(app)
+
+class TestHealthcheck:
+    @pytest.mark.it('Check connects to server')
+    def test_connects_to_server(self, test_client):
+        result = test_client.get('/')
+        assert result.status_code == 200
+        assert result.json() == {"message": 'Everything OK'}
